bb660b45240615116795a3c30256ac302bf6278e
Evan Goodberry <egoodberry@gmail.com>
2012-11-26 23:25:25 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MikeOdmark::Application.config.secret_token = '1f3ca6f2f93b142bca22a75b9d6d249801ab3fc1d2c45ef0c9dbd4c050cd5a8b9c3f8ad2b87ed91033fecc872ffa88bd1b2b6981c6b22e04d2652c3b21a70e5b'
