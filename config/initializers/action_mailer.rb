ec33d1d701132fc4a6dd3392b5bbea8ae45dc992
Evan Goodberry <egoodberry@gmail.com>
2012-12-01 21:48:35 -0500

    Use Gmail for sending messages, add favicon.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,7 +1,7 @@
-ActionMailer::Base.smtp_settings = {
-  :address  => "smtp.sendgrid.net",
-  :port  => 587,
-  :user_name  => "app9528558@heroku.com",
-  :password  => ENV["SENDGRID_PASSWORD"],
-  :authentication  => :plain
-}
+# ActionMailer::Base.smtp_settings = {
+  # :address  => "smtp.sendgrid.net",
+  # :port  => 587,
+  # :user_name  => "app9528558@heroku.com",
+  # :password  => ENV["SENDGRID_PASSWORD"],
+  # :authentication  => :plain
+# }
