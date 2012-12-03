bc6abeb59c89f684fdfe549b0be18743b25e94c5
Evan Goodberry <egoodberry@gmail.com>
2012-12-02 23:39:12 -0500

    Enable responsiveness & HTML editing.

--- app/assets/javascripts/load.js
+++ app/assets/javascripts/load.js
@@ -1,5 +1,7 @@
 $(function() {
-  $('.html-editor').wysihtml5();
+  $('.html-editor').wysihtml5({
+    html: true
+  });
 
   $('.html-output img').each(function() {
     $(this).wrap('<span class="photo">');
