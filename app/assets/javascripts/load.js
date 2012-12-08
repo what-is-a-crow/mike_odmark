7981679858a2bb0cce46850cd6cdcdef70bf0742
Evan Goodberry <egoodberry@gmail.com>
2012-12-08 18:50:10 -0500

    Allow iframes.

--- app/assets/javascripts/load.js
+++ app/assets/javascripts/load.js
@@ -1,6 +1,23 @@
 $(function() {
-  $('.html-editor').wysihtml5({
-    html: true
+  $('.html-editor').wysihtml5('deepExtend', {
+    html: true,
+    parserRules: {
+      tags: {
+        iframe: {
+          "check_attributes": {
+            "src":"url"
+          },
+          "set_attributes": {
+            "width": "560",
+            "height": "315",
+            "frameborder": "0",
+            "allowfullscreen": ""
+          }
+        },
+        object: {},
+        embed: {}
+      }
+    }
   });
 
   $('.html-output img').each(function() {
